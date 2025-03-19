0692d274a94b5a1d94885fb4b5ba734ab72fa201
Mark de Neut Brossois <mark.dnb@live.nl>
2025-03-19 20:41:11 +0100

    first commit

---
+++ pages/vote.vue
@@ -0,0 +1,10 @@
+<template>
+    <UContainer class="flex flex-col gap-4 justify-center items-center w-full h-screen lg:flex-row">
+        <ClientOnly>
+            <Connect />
+        </ClientOnly>
+    </UContainer>
+</template>
+
+<script setup lang="ts">
+</script>
